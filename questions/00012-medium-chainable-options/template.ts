1342bf66dee53fd3b6a54024c4a4f20bf4a4759b
NeilChenCN <neil.chen.nj@gmail.com>
2022-07-09 20:21:00 +0800

    answer: NO.12 chainable options

--- questions/00012-medium-chainable-options/template.ts
+++ questions/00012-medium-chainable-options/template.ts
@@ -1,4 +1,4 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable<O extends {}= {}> = {
+  option<key extends string, value>(key: key extends keyof O ? never : key, value: value): Chainable<O & { [k in key]: value }>
+  get(): O
 }
