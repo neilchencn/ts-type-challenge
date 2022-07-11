7d3ec9ebafb0d256670370879d12af2188546cf0
NeilChenCN <neil.chen.nj@gmail.com>
2022-07-11 15:22:36 +0800

    answer: NO.20 promise all

--- questions/00020-medium-promise-all/template.ts
+++ questions/00020-medium-promise-all/template.ts
@@ -1,1 +1,1 @@
-declare function PromiseAll(values: any): any
+declare function PromiseAll<T extends any[]>(values: readonly [...T]): Promise<{ [K in keyof T]: T[K] extends Promise<infer R> ? R : T[K] }>
