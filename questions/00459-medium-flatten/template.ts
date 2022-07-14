db4fae3cbeaad918d54a0ec9fd82f65058fa811c
NeilChenCN <neil.chen.nj@gmail.com>
2022-07-14 10:20:16 +0800

    answer: NO.459 Flatten

--- questions/00459-medium-flatten/template.ts
+++ questions/00459-medium-flatten/template.ts
@@ -1,1 +1,1 @@
-type Flatten = any
+type Flatten<T extends any[]> = T extends [infer First, ...infer Rest] ? First extends any[] ? [...Flatten<First>, ...Flatten<Rest>] : [First, ...Flatten<Rest>] : T
