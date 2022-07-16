5b7c6d03d1965661d6af2cb19fdcdc60ac86ce3e
NeilChenCN <neil.chen.nj@gmail.com>
2022-07-16 21:09:34 +0800

    answer: NO.949 anyof

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,1 +1,2 @@
-type AnyOf<T extends readonly any[]> = any
+type Falsy = 0 | '' | [] | false | Record<string, never>
+type AnyOf<T extends readonly any[]> = T extends [infer Pre, ...infer Rest] ? Pre extends Falsy ? AnyOf<Rest> : true : false
