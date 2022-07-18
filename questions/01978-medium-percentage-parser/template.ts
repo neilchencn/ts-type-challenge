1abc272d98e1e3caa6d17c1f44079ba960611bfa
NeilChenCN <neil.chen.nj@gmail.com>
2022-07-18 11:09:40 +0800

    answer: NO.1978 percentage parser

--- questions/01978-medium-percentage-parser/template.ts
+++ questions/01978-medium-percentage-parser/template.ts
@@ -1,1 +1,4 @@
-type PercentageParser<A extends string> = any
+type Pre<A extends string> = A extends `${infer pre extends '+' | '-'}${infer _}` ? pre : ''
+type Suf<A extends string> = A extends `${infer _}%` ? '%' : ''
+type Mid<A extends string> = A extends `${Pre<A>}${infer rest}${Suf<A>}` ? rest : ''
+type PercentageParser<A extends string> = [Pre<A>, Mid<A>, Suf<A>]
