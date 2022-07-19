02190127e690ab20ee8c8cfc3216cb436c116f4d
NeilChenCN <neil.chen.nj@gmail.com>
2022-07-19 14:08:44 +0800

    answer: NO.2257 minus one

--- questions/02257-medium-minusone/template.ts
+++ questions/02257-medium-minusone/template.ts
@@ -1,1 +1,5 @@
-type MinusOne<T extends number> = any
+type ArrLength<N extends string, T extends any[] = []> = `${T['length']}` extends N ? T : ArrLength<N, [any, ...T]>
+
+type CreateLengthArray<S extends string, T extends any[] = []> = S extends `${infer F}${infer L}` ? CreateLengthArray<L, [...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...ArrLength<F>]> : T
+
+type MinusOne<T extends number> = CreateLengthArray<`${T}`> extends [infer _, ...infer Rest] ? Rest['length'] : never
