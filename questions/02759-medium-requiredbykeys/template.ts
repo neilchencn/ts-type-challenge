4290c1d57a4c10b2a9ca9463f76cc3cfb8d5fbae
NeilChenCN <neil.chen.nj@gmail.com>
2022-07-25 09:51:44 +0800

    answer: NO.2759 require by keys

--- questions/02759-medium-requiredbykeys/template.ts
+++ questions/02759-medium-requiredbykeys/template.ts
@@ -1,1 +1,8 @@
-type RequiredByKeys<T, K> = any
+type mergeAgain<T> = {
+  [P in keyof T]: T[P]
+}
+
+type RequiredByKeys<T, K=keyof T> = mergeAgain<{
+  [P in keyof T as P extends K ? P : never]-?: T[P]
+
+} & { [P in keyof T as P extends MyExclude<keyof T, K> ? P : never]: T[P] }>
