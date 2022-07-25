043eda30c2530eefe855de2217e3a1e218900259
NeilChenCN <neil.chen.nj@gmail.com>
2022-07-25 09:51:22 +0800

    answer: NO.2757 partial by keys

--- questions/02757-medium-partialbykeys/template.ts
+++ questions/02757-medium-partialbykeys/template.ts
@@ -1,1 +1,8 @@
-type PartialByKeys<T, K> = any
+type merge<T> = {
+  [P in keyof T]: T[P]
+}
+
+type PartialByKeys<T, K = keyof T> = merge<{
+  [P in keyof T as P extends K ? P : never]?: T[P]
+
+} & { [P in keyof T as P extends MyExclude<keyof T, K> ? P : never]: T[P] }>
