de0e92ceddf32feefa4b1d6644e04f0e02ca105c
NeilChenCN <neil.chen.nj@gmail.com>
2022-08-10 13:54:55 +0800

    answer: No.3243 flatten depth

--- questions/03243-medium-flattendepth/template.ts
+++ questions/03243-medium-flattendepth/template.ts
@@ -1,1 +1,7 @@
-type FlattenDepth = any
+type MyFlatten<T extends any[], R extends any[] = []> = T extends [infer T, ...infer Rest]
+  ? (T extends [...infer P] ? MyFlatten<[...Rest], [...R, ...P]> : MyFlatten<[...Rest], [...R, T]>)
+  : R
+
+type FlattenDepth<T extends any[], D = 1, R extends any[] = []> = MyFlatten<T> extends T
+  ? T
+  : (R['length'] extends D ? T : FlattenDepth<MyFlatten<T>, D, [...R, 0]>)
