02b3332acaad87fc579e7918bccdcf61e2b3874c
NeilChenCN <neil.chen.nj@gmail.com>
2022-08-12 15:10:51 +0800

    answer: No.3326 bem style string

--- questions/03326-medium-bem-style-string/template.ts
+++ questions/03326-medium-bem-style-string/template.ts
@@ -1,1 +1,5 @@
-type BEM<B extends string, E extends string[], M extends string[]> = any
+type BEM<B extends string, E extends string[], M extends string[]> = E extends []
+  ? `${B}--${M[number]}`
+  : M extends []
+    ? `${B}__${E[number]}`
+    : `${B}__${E[number]}--${M[number]}`
